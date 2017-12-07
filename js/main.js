352f686b95a6e93c7c3aebe5a49985ea874d799b
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2017-12-07 15:50:38 +0100

    testimonial slider dodat

--- js/main.js
+++ js/main.js
@@ -1,97 +1,124 @@
-$(document).ready(function(){
-    
-    $(window).scroll(function(){
-       var scroll = $(window).scrollTop();
-       if(scroll > 150 ){
-           $('header').addClass('py-2');
-       } else{
-         $('header').removeClass('py-2');  
-       }
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 150) {
+            $('header').addClass('py-2');
+        } else {
+            $('header').removeClass('py-2');
+        }
     });
-    
+
     //spy-scroll
-    
-    $('.navbar-nav a').click(function(){
+
+    $('.navbar-nav a').click(function () {
         $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
     });
-    
+
     //bx slider
-    
-    if ($('.bxslider').length > 0 ){
+
+    if ($('.bxslider').length > 0) {
         $('.lead-slider').bxSlider();
+        $('.testimonial-slider').bxSlider();
     }
-    
+
     //animacije
-    
-    function animation(){
+
+    function animation() {
         var windowHeight = $(window).height();
         var scroll = $(window).scrollTop();
-        $('.animation').each(function(){
+        $('.animation').each(function () {
             var pozicija = $(this).offset().top;
             var animacija = $(this).attr('data-animation');
-            if( pozicija < scroll + windowHeight - 100){
+            if (pozicija < scroll + windowHeight - 100) {
                 $(this).addClass(animacija);
             }
         });
     }
-    
+
     animation();
-    
-    $(window).scroll(function(){
+
+    $(window).scroll(function () {
         animation();
     });
-    
-    
+
+
     //owl
-    
-    if($('.owl-carousel').length > 0){
+
+    if ($('.owl-carousel').length > 0) {
         $('.team-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        992:{
-            items:4
-        }
-    }
-});
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
 
-    $('.portfolio-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        992:{
-            items:4
-        },
-        1200:{
-            items:5
-        }
-    }
-});
+        $('.portfolio-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+            }
+        });
 
     }
-    
-    
-    
+
+
+
+    //counter
+
+    $(window).scroll(function () {
+        var vh = $(window).height();
+        var scroll = $(window).scrollTop();
+        var position = $('.achievements').offset().top;
+        $('.counter-number').each(function () {
+
+            if (position < vh + scroll - 100) {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+
+                });
+            }
+            ;
+        });
+    });
+
+
+
 });
